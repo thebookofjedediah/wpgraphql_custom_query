9bf4bab2bde7983bc3b12fb20625f1b9eb581915
Jedediah Arnold <jarnold@JArnold-008110.local>
2019-06-28 11:17:24 -0400

    Jason did it

--- attachment_query.php
+++ attachment_query.php
@@ -1,10 +1,17 @@
-add_action( 'graphql_register_types', function() {
-  register_graphql_field( 'Post', 'attachment', [
-     'type' => 'DataSource',
-     'description' => __( 'Attachments on the post', 'wp-graphql' ),
-     'resolve' => function( $post ) {
-       $attachment = get_post_meta( $post->ID, 'attachment', true );
-       return ! empty( $attachment ) ? $attachment : 'null';
-     }
-  ] );
-} );
+add_action( 'graphql_register_types', 'register_my_custom_graphql_connection', 99 );
+
+function register_my_custom_graphql_connection() {
+    $config = [
+    'fromType' => 'RootQuery',
+    'toType' => 'Post',
+    'fromFieldName' => 'postsAttachments',
+    'connectionTypeName' => 'PostsAttachments',
+    'resolve' => function( $id, $args, $context, $info ) {
+        return \WPGraphQL\Data\DataSource::resolve_post_objects_connection( $id, $args, $context, $info, 'post' );
+    },
+    'resolveNode' => function( $id, $args, $context, $info ) {
+        return \WPGraphQL\Data\DataSource::resolve_post_object( $id, $context );
+    }
+    ];
+    register_graphql_connection( $config );
+};
